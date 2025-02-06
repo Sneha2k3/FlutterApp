9cdbd118e20574b89e26e03f1d28e269df95a845
Sneha2k3 <125958986+Sneha2k3@users.noreply.github.com>
2025-02-06 13:15:55 +0545

    di

--- lib/app/di/di.dart
+++ lib/app/di/di.dart
@@ -1,26 +1,39 @@
-import 'package:get_it/get_it.dart';
-
+import '../../core/network/api_service.dart';
 import '../../core/network/hive_service.dart';
-import '../../features/auth/data/data_source/local_data_source/auth_local_data_source.dart';
-import '../../features/auth/data/repository/auth_local_repository.dart';
+import '../../core/network/hive_service.dart';
+import '../../features/auth/data/repository/auth_local_repository/auth_local_repository.dart';
+import '../../features/auth/domain/use_case/login_usecase.dart';
 import '../../features/auth/domain/use_case/login_usecase.dart';
 import '../../features/auth/domain/use_case/register_user_usecase.dart';
+import '../../features/auth/domain/use_case/register_user_usecase.dart';
+import '../../features/auth/presentation/view_model/login/login_bloc.dart';
 import '../../features/auth/presentation/view_model/login/login_bloc.dart';
 import '../../features/auth/presentation/view_model/signup/register_bloc.dart';
+import '../../features/auth/presentation/view_model/signup/register_bloc.dart';
 import '../../features/home/presentation/view_model/home_cubit.dart';
-
 
 final getIt = GetIt.instance;
 
 Future<void> initDependencies() async {
   // First initialize hive service
   await _initHiveService();
-
-  await _initHomeDependencies();
+  await _initDashboardDependencies();
   await _initRegisterDependencies();
   await _initLoginDependencies();
+  await _initApiService();
+  await _initSharedPreferences();
+}
 
-  // await _initSplashScreenDependencies();
+Future<void> _initSharedPreferences() async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+}
+
+_initApiService() {
+  //Remote Data Source
+  getIt.registerLazySingleton<Dio>(
+    () => ApiService(Dio()).dio,
+  );
 }
 
 _initHiveService() {
@@ -33,49 +46,70 @@ _initRegisterDependencies() {
     () => AuthLocalDataSource(getIt<HiveService>()),
   );
 
+  getIt.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSource(getIt<Dio>()),
+  );
+
   // init local repository
   getIt.registerLazySingleton(
     () => AuthLocalRepository(getIt<AuthLocalDataSource>()),
   );
 
+  getIt.registerLazySingleton(
+    () => AuthRemoteRepository(getIt<AuthRemoteDataSource>()),
+  );
+
   // register use usecase
   getIt.registerLazySingleton<RegisterUseCase>(
     () => RegisterUseCase(
-      getIt<AuthLocalRepository>(),
+      getIt<AuthRemoteRepository>(),
+    ),
+  );
+
+  getIt.registerLazySingleton<UploadImageUseCase>(
+    () => UploadImageUseCase(
+      getIt<AuthRemoteRepository>(),
     ),
   );
 
   getIt.registerFactory<RegisterBloc>(
     () => RegisterBloc(
       registerUseCase: getIt(),
+      uploadImageUseCase: getIt(),
     ),
   );
 }
 
-_initHomeDependencies() async {
-  getIt.registerFactory<HomeCubit>(
-    () => HomeCubit(),
+_initDashboardDependencies() async {
+  getIt.registerFactory<DashboardCubit>(
+    () => DashboardCubit(),
   );
 }
 
 _initLoginDependencies() async {
+  // =========================== Token Shared Preferences ===========================
+  getIt.registerLazySingleton<TokenSharedPrefs>(
+    () => TokenSharedPrefs(getIt<SharedPreferences>()),
+  );
+
   getIt.registerLazySingleton<LoginUseCase>(
     () => LoginUseCase(
-      getIt<AuthLocalRepository>(),
+      getIt<AuthRemoteRepository>(),
+      getIt<TokenSharedPrefs>(),
     ),
   );
 
   getIt.registerFactory<LoginBloc>(
     () => LoginBloc(
       registerBloc: getIt<RegisterBloc>(),
-      homeCubit: getIt<HomeCubit>(),
+      dashboardCubit: getIt<DashboardCubit>(),
       loginUseCase: getIt<LoginUseCase>(),
     ),
   );
 }
 
-// _initSplashScreenDependencies() async {
-//   getIt.registerFactory<SplashCubit>(
-//     () => SplashCubit(getIt<LoginBloc>()),
-//   );
-// }
+_initOnboardingScreenDependencies() async {
+  getIt.registerFactory<OnboardingCubit>(
+    () => OnboardingCubit(getIt<LoginBloc>()),
+  );
+}
