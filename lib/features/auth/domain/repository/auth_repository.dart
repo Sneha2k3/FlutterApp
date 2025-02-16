320c8c87a143081eaf98b95dee4d3150ffcbc54c
Sneha2k3 <125958986+Sneha2k3@users.noreply.github.com>
2025-02-16 22:35:41 +0545

    auth_repo

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -12,4 +12,6 @@ abstract interface class IAuthRepository {
       String username, String password);
 
   Future<Either<Failure, String>> uploadProfilePicture(File file);
+
+  Future<Either<Failure, AuthEntity>> getCurrentUser();
 }
