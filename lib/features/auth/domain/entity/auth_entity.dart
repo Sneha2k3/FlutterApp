93042bea00117afefd7117643dc67afff24d4ffe
Sneha2k3 <125958986+Sneha2k3@users.noreply.github.com>
2025-02-16 22:30:08 +0545

    auth

--- lib/features/auth/domain/entity/auth_entity.dart
+++ lib/features/auth/domain/entity/auth_entity.dart
@@ -8,18 +8,4 @@ class AuthEntity extends Equatable {
   final String phone;
   final String username;
   final String password;
-
-  const AuthEntity({
-    this.userId,
-    required this.firstName,
-    required this.lastName,
-    this.image,
-    required this.phone,
-    required this.username,
-    required this.password,
-  });
-
-  @override
-  List<Object?> get props =>
-      [userId, firstName, lastName, image, username, password, phone];
 }
