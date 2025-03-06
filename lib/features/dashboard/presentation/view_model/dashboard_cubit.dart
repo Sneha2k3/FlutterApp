6ee9402299ef3bd7490837fdf12f29b85b47649d
Sneha2k3 <125958986+Sneha2k3@users.noreply.github.com>
2025-03-06 17:12:15 +0545

    dashboardC,S

--- lib/features/dashboard/presentation/view_model/dashboard_cubit.dart
+++ lib/features/dashboard/presentation/view_model/dashboard_cubit.dart
@@ -1,5 +1,5 @@
+import 'package:features/dashboard/presentation/view_model/dashboard_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wheeloop/features/dashboard/presentation/view_model/dashboard_state.dart';
 
 class DashboardCubit extends Cubit<DashboardState> {
   DashboardCubit() : super(DashboardState.initial());
--- lib/features/dashboard/presentation/view_model/dashboard_state.dart
+++ lib/features/dashboard/presentation/view_model/dashboard_state.dart
@@ -1,47 +1,22 @@
-import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wheeloop/app/service_locator/service_locator.dart';
-import 'package:wheeloop/features/dashboard/presentation/view/bottom_view/home_view.dart';
-import 'package:wheeloop/features/dashboard/presentation/view_model/home_cubit.dart';
-
-class DashboardState extends Equatable {
-  final int selectedIndex;
+class DashboardState {
   final List<Widget> views;
+  final int selectedIndex;
 
-  const DashboardState({
-    required this.selectedIndex,
-    required this.views,
-  });
+  DashboardState({required this.views, required this.selectedIndex});
 
-  // Initialize the state with the default values
-  static DashboardState initial() {
+  // Initial state method
+  factory DashboardState.initial() {
     return DashboardState(
-      selectedIndex: 0,
-      views: [
-        BlocProvider<HomeCubit>.value(
-          value: serviceLocator<
-              HomeCubit>(), // Use the GetIt service locator to provide HomeCubit instance
-          child: const HomeScreen(), // HomeScreen which requires HomeCubit
-        ),
-        const Center(child: Text('Saved Car')),
-        const Center(child: Text('Notification IS HERO IS THE')),
-        const Center(child: Text('Profile')),
-      ],
+      views: [HomeView(), AppointmentView(), ProfileView()], // Your views
+      selectedIndex: 0, // Initial index
     );
   }
 
-  // Create a copy of the state with updated values
-  DashboardState copyWith({
-    int? selectedIndex,
-    List<Widget>? views,
-  }) {
+  // Copy method for updating state
+  DashboardState copyWith({List<Widget>? views, int? selectedIndex}) {
     return DashboardState(
-      selectedIndex: selectedIndex ?? this.selectedIndex,
       views: views ?? this.views,
+      selectedIndex: selectedIndex ?? this.selectedIndex,
     );
   }
-
-  @override
-  List<Object?> get props => [selectedIndex, views];
 }
